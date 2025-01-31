b9bd1760ec3483cf772257f01db237cd56b102f7
Maria Aluko <maria.alukoo@gmail.com>
2025-01-31 10:28:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <Button>Hello</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
